38009b36d2e95ff8ddbbf2a479c2491793a388cd
Stanley <125638862+a1103317@users.noreply.github.com>
2023-02-20 19:17:05 +0800

    Add files via upload

---
+++ a1103317.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+A1103317_homework1</br>
+
+<title>a1103317</title>
+
+</head>
+<body text="blue">
+
+<?php
+echo "I'm handsome.</br>";
+echo "I want to be a hardworking student.</br>"
+?>
+
+</body>
+</html>
