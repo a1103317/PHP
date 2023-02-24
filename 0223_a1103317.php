2768eab2dce01873bf03fd1067db9c6dd9ab470f
Stanley <125638862+a1103317@users.noreply.github.com>
2023-02-25 00:28:35 +0800

    Add files via upload

---
+++ 0223_a1103317.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8"> 
+    <title>來夯肉！</title>
+</head>
+
+<body text="black" background="img/bbq.jpg">       <!-- bgcolor背景顏色 background背景圖片 -->
+
+    <center>
+    <font size="6" color="black" face="Calibri">            <!-- font 文字設定-->
+    <b>NUKIM系烤報名表</b><hr>
+    </font>
+    </center> 
+
+    <font size="4" color="black" face="Calibri">
+    <b>主旨：</b></br>
+    又到了一年一度的系烤時間啦~~~🍖🍖</br>
+    這是資管系每年都會舉辦的活動之一</br>
+    目的是為了藉由烤肉來增進感情🥵🥵<p>
+    </font>
+
+    <font size="4" color="black" face="Calibri">
+    <b>活動詳情與注意事項👇🏻</b></br>
+    </font>
+
+    <font size="3" color="black" face="Menlo">
+    date : 2023/3/22</br>
+    time : 17:00 ~ 21:00</br>
+    price : 600 元/人</br>
+    place : 宿舍烤肉區 </br>
+    每組人數 : 6 人</br>
+    需自備餐具 當個環保小公民✅</br>
+    ❌浪費食物</br>
+    ❌亂丟垃圾</br>
+    ❌玩火<p>
+
+    </font>            
+
+    <font size="4" color="black" face="Calibri">
+    <b>當日食材如下：</b>
+    </font>
+
+    <table border = "2" bgcolor="white">
+
+        <tr><th>類別</th><th>食材</th><th>數量</th><tr>
+        <tr><th>肉</th><td>日本飛驒牛🐂</td><td>1盒</td><tr>
+        <tr><th>肉</th><td>Prime無骨牛小排🐂</td><td>1盒</td><tr>
+        <tr><th>肉</th><td>伊比利豬🐷</td><td>1盒</td><tr>
+        <tr><th>肉</th><td>松阪豬🐷</td><td>1盒</td><tr>
+        <tr><th>肉</th><td>無骨雞腿排🐔</td><td>3片</td><tr>
+        <tr><th>菜</th><td>青椒、玉米筍🫑</td><td>各2份</td><tr> 
+        <tr><th>菜</th><td>四季豆、櫛瓜🥒</td><td>各2份</td><tr>    
+        <tr><th>菜</th><td>地瓜🍠、玉米🌽</td><td>各2份</td><tr>
+        <tr><th>海鮮</th><td>廣島大牡蠣🦪</td><td>5顆</td><tr> 
+        <tr><th>海鮮</th><td>天使紅蝦🦐</td><td>5隻</td><tr>
+        <tr><th>海鮮</th><td>澎湖縣撈小捲🦑</td><td>2隻</td><tr>
+        <tr><th>海鮮</th><td>波士頓龍蝦🦞</td><td>2隻</td><tr>
+        <tr><th>其他</th><td>米血、甜不辣</td><td>各5片</td><tr>   
+
+    </table></p>
+
+    <font size="4" color="black" face="Calibri">
+    <b>報名資料：📝</b>
+    </font>
+
+    <font size="4" color="black" face="Calibri">
+    <form action="Stanleyß.php" method="post">
+
+        請輸入您的學號：<input type="text" name="id" placeholder="開頭請大寫" value="" required></br>
+        </p>
+        請輸入您的姓名：<input type="text" name="id"></br>
+        </p>
+        我吃肉<input type="checkbox" name="meat eater" checked>
+        我吃草<input type="checkbox" name="vegetarian">
+        </p>
+        一定到<input type="checkbox" name="yes">
+        下次一定<input type="checkbox" name="no">
+        </br>
+        我有話想說：
+        <input type="text"></p>
+
+        <font size="4" color="black" face="Calibri"><b>115級臉書社團：</b></font>
+        <a href="https://www.facebook.com/groups/1779155919095744/permalink/1940419209636080/" target="nuk">系烤活動</a></p>
+        
+        <input type="submit">
+        <input type="reset">
+       
+        
+    </form>
+    </font>
+
+</body>
+
+<font size="1" color="orange" face="Calibri">
+我太累ㄌ🫠
+</font>
+
+</html>
